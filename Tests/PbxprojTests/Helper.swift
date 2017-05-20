50fa6bc4ba0a2f60c0c76aa69db0e92346811f88
toshi0383 <t.suzuki326@gmail.com>
2017-05-20 18:21:05 +0900

    Add Test Helper to create files and dirs

--- Tests/PbxprojTests/Helper.swift
+++ Tests/PbxprojTests/Helper.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Pbxproj
+import PathKit
 
 func pathForFixture(fileName: String) -> String {
     #if Xcode
@@ -17,4 +18,15 @@ func _pbxproj() -> Pbxproj {
         let path = "Tests/PbxprojTests/test.pbxproj"
     #endif
     return try! Pbxproj(path: path)
+}
+
+func createPathAndFiles(path: String) {
+    let components = path.components(separatedBy: "/")
+    let dir = components.dropLast()
+    let d = Path(components: dir)
+    if d.exists == false {
+        try! d.mkpath()
+    }
+    let file = components.last!
+    try! (d + file).write("")
 }
